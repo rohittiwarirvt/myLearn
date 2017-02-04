17e55ba316f1bb33143c75358952c2695d436d53
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-02-04 16:29:44 +0530

    Added zizaco entrust

---
+++ myLaravelApps/blog/vendor/zizaco/entrust/src/Entrust/Entrust.php
@@ -0,0 +1,12 @@
+<?php namespace Zizaco\Entrust;
+
+/**
+ * This class is the main entry point of entrust. Usually this the interaction
+ * with this class will be done through the Entrust Facade
+ *
+ * @license MIT
+ * @package Zizaco\Entrust
+ */
+
+class Entrust
+{
